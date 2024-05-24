bdd09d624553344e20b7f3812ad32abfcf6aec49
Prashant <prashantchapagain@gmail.com>
2024-05-24 16:38:47 +0545

    Created model class for DB Tables

---
+++ ResturantAPI/Models/OrderDetail.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResturantAPI.Models;
+
+public class OrderDetail
+{
+    [Key]
+    public long OrderDetailId { get; set; }
+    public long OrderMasterId { get; set; }
+    public int FoodItemId { get; set; }
+    public decimal FoodItemPrice { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ ResturantAPI/Models/OrderMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResturantAPI.Models;
+
+public class OrderMaster
+{
+    [Key]
+    public long OrderMasterId { get; set; }
+
+    [Column(TypeName = "nvarchar(30)")]
+    public string OrderNumber { get; set; }
+    public int CustomerId { get; set; }
+
+    [Column(TypeName = "nvarchar(30)")]
+    public string PaymentMethod { get; set; }
+    public decimal GrandTotal { get; set; }
+}
